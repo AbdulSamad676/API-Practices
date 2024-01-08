4bb31205bd200a159a4eb9309482022450b1ae92
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-08 09:25:04 +0500

    API data fatched

--- javaScript.js
+++ javaScript.js
@@ -1,2 +1,17 @@
-let xhr = new HttpRequest();
-const url = xhr.open('Get');
+const requestUrl = 'https://api.github.com/users/hiteshchoudhary';
+let xhr = new XMLHttpRequest(); // creating xml object
+const url = xhr.open('Get', requestUrl); //sending request to the server
+let data;
+// onreadystatechange takes a function to be tracked not itself a function
+xhr.onreadystatechange = function () {
+	console.log(xhr.readyState);
+
+	if (xhr.readyState === 4) {
+		data = JSON.parse(this.responseText);
+		// actually hamy data string me milta hai es liye ham JSON.parse use kar k object format me convert kary hai
+		// console.log(data.followers); // this actually refers to the current object
+	}
+	console.log(data);
+};
+
+xhr.send(); // this method call the open method
