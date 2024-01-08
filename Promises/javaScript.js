faef904b6b6e10b210919682c12f5d24173e8c3e
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-08 12:03:29 +0500

    Async await added

--- Promises/javaScript.js
+++ Promises/javaScript.js
@@ -1,13 +1,36 @@
-let promise = new Promise(function (resolve, reject) {
+let promiseOne = new Promise(function (resolve, reject) {
 	setTimeout(function () {
 		console.log('Promise Created successfully');
 		resolve();
 	}, 1000);
 });
-promise.then(function () {
+promiseOne.then(function () {
 	console.log('Promises resolved');
 });
-promise.catch(function () {});
-promise.finally(function () {
+promiseOne.catch(function () {});
+promiseOne.finally(function () {
 	console.log('the promise is consumed either resolved or rejected');
 });
+// second approach
+
+let error = false;
+let promiseTwo = new Promise(function (resolve, reject) {
+	setTimeout(function () {
+		if (!error) {
+			resolve({ name: 'Abdul Samad Khan', age: 24 });
+		} else {
+			reject('ERR: Something is wrong');
+		}
+	}, 1000);
+});
+
+async function consumePromiseTwo() {
+	try {
+		const response = await promiseTwo;
+		console.log(response);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+consumePromiseTwo();
