d668321fff5aa9b7e21dbfac595c4d1cba2365c7
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-09 15:59:15 +0500

    data fatched successfully

--- Get-And-Display-API-Data/javaScript.js
+++ Get-And-Display-API-Data/javaScript.js
@@ -1,1 +1,27 @@
 console.log('Environment setup');
+
+// const response = fetch('https://jsonplaceholder.typicode.com/users')
+// 	.then(function (response) {
+// 		let data = response.json();
+// 		console.log(data.Array);
+// 	})
+// 	.catch(function (error) {
+// 		console.log(`ERR: ${error}`);
+// 	});
+
+function display(data) {
+	// console.log(data);
+	data.forEach(item => {
+		console.log(item);
+	});
+}
+
+fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response => {
+		return response.json();
+	})
+	.then(data => {
+		// console.log(data);
+		display(data);
+	})
+	.catch(error => console.log(error));
